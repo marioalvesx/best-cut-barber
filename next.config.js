79c96f0c58681c3ad0feba8616f320e87d0382ac
Mario Alves <marioalvesneto@hotmail.com>
2025-10-04 22:08:50 -0300

    fix: Forces verification after build

--- next.config.js
+++ next.config.js
@@ -14,6 +14,9 @@ const nextConfig = {
     ignoreBuildErrors: false,
   },
   images: { unoptimized: true },
+  generateBuildId: async () => {
+    return "build-" + Date.now();
+  },
 };
 
 module.exports = nextConfig;
