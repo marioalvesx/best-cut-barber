01b9b1ef2348e5a6670ac1f9b2b60742fc2b064c
Mario Alves <marioalvesneto@hotmail.com>
2025-10-04 22:13:16 -0300

    fix: Comment nex.config.js

--- next.config.js
+++ next.config.js
@@ -2,21 +2,21 @@ const path = require("path");
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  distDir: process.env.NEXT_DIST_DIR || ".next",
-  output: process.env.NEXT_OUTPUT_MODE,
-  experimental: {
-    outputFileTracingRoot: path.join(__dirname, "../"),
-  },
-  eslint: {
-    ignoreDuringBuilds: true,
-  },
-  typescript: {
-    ignoreBuildErrors: false,
-  },
-  images: { unoptimized: true },
-  generateBuildId: async () => {
-    return "build-" + Date.now();
-  },
+  // distDir: process.env.NEXT_DIST_DIR || ".next",
+  // output: process.env.NEXT_OUTPUT_MODE,
+  // experimental: {
+  //   outputFileTracingRoot: path.join(__dirname, "../"),
+  // },
+  // eslint: {
+  //   ignoreDuringBuilds: true,
+  // },
+  // typescript: {
+  //   ignoreBuildErrors: false,
+  // },
+  // images: { unoptimized: true },
+  // generateBuildId: async () => {
+  //   return "build-" + Date.now();
+  // },
 };
 
 module.exports = nextConfig;
